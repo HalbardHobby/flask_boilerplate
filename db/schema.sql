0e5f4fd9d22458db77677e5df68b721c568be927
Alejandro <HalbardHobby@users.noreply.github.com>
2023-05-06 17:03:40 -0500

    🗃️  Added default sql dump

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS data;
+USE data
+
+DROP TABLE IF EXISTS `customers`;
+
+CREATE TABLE `customers` (
+  `customerNumber` int(11) NOT NULL,
+  `customerName` varchar(50) NOT NULL,
+  `contactLastName` varchar(50) NOT NULL,
+  `contactFirstName` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `addressLine1` varchar(50) NOT NULL,
+  `addressLine2` varchar(50) default NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) default NULL,
+  `postalCode` varchar(15) default NULL,
+  `country` varchar(50) NOT NULL,
+  `salesRepEmployeeNumber` int(11) default NULL,
+  `creditLimit` double default NULL,
+  PRIMARY KEY  (`customerNumber`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
